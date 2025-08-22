a71706c9f807bc3b3988e1c9f975937fccd39c44
Jatin Dua <jdua004@gmail.com>
2025-08-22 23:25:34 +0530

    feat: Implement Add School API

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { pool } from "../config/db.js";
+
 const router = Router();
 
 // Define routes
@@ -6,8 +8,20 @@ router.get('/listSchools', (req, res) => {
   res.send('List of schools');
 });
 
-router.post('/addSchool', (req, res) => {
-  res.send('Add a new school');
+router.post("/addSchool", async (req, res) => {
+  try {
+    const { name, address, latitude, longitude } = req.body;
+
+    const [result] = await pool.query(
+      "INSERT INTO schools (name, address, latitude, longitude) VALUES (?, ?, ?, ?)",
+      [name, address, latitude, longitude]
+    );
+
+    res.status(201).json({ id: result.insertId, name, address, latitude, longitude });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Error adding school" });
+  }
 });
 
 export default router;
