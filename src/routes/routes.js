32de3fcf172d02087c64cf0c11eb1af2f27edc09
Jatin Dua <jdua004@gmail.com>
2025-08-25 01:29:59 +0530

    feat: Add json field length validation

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -14,8 +14,8 @@ router.get("/listSchools",
 
 router.post("/addSchool",
   [
-    body("name").isString().trim().notEmpty().withMessage("School name required"),
-    body("address").isString().trim().notEmpty().withMessage("Address required"),
+    body("name").isString().trim().isLength({ min: 1, max: 100}).withMessage("School name required"),
+    body("address").isString().trim().isLength({ min: 1, max: 200}).withMessage("Address required"),
     body("latitude").isFloat({ min: -90, max: 90 }).withMessage("Invalid latitude"),
     body("longitude").isFloat({ min: -180, max: 180 }).withMessage("Invalid longitude"),
   ],
